31cdf922e45f5c5ab6d162fb0564d79f1d6cf931
nyandago <paulmutator@gmail.com>
2021-08-22 10:04:58 +0300

    Redesigned with Constraint Layout

--- app/src/main/java/com/cannybits/calculator/MainActivity.kt
+++ app/src/main/java/com/cannybits/calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.cannybits.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun btNumberEvent(view: View){
+
     }
 }
