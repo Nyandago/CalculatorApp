14213f8b79b27aa6278176b44fa925d49f2c0ee0
nyandago <paulmutator@gmail.com>
2021-08-22 11:56:07 +0300

    Redesigned with Constraint Layout

--- app/src/main/java/com/cannybits/calculator/MainActivity.kt
+++ app/src/main/java/com/cannybits/calculator/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    private fun btNumberEvent(view: View){
+    fun btNumberEvent(view: View){
 
     }
 }
